6d39760604734edfd8676b096ff55d86cadf7ed0
Miloiu Raluca-Maria <miloiuralucamaria@gmail.com>
2026-04-05 18:18:13 +0000

    Adaugare: afisare Shape Ratio cu 3 zecimale(trunchiere)

--- src/tema.c
+++ src/tema.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
     FILE *in = fopen(argv[1],"rt");
     FILE *out = fopen(argv[2],"wt");
-    double valoare_anterioara = 0, rand_mediu = 0, volatilitate = 0;
+    double valoare_anterioara = 0, rand_mediu = 0, volatilitate = 0, S;
     int n;
     N* cap = NULL;
     N* p = NULL;
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
     for(int incercare = 0; incercare<n; incercare++)
     {
         N* nou = (N*)malloc(sizeof(N));
-        fscanf(in, "%lf",&nou->valoare);
+        fscanf(in,"%lf",&nou->valoare);
         nou->randament = 0;
         nou->urm = NULL;
         if(cap == NULL)
@@ -55,10 +55,12 @@ int main(int argc, char *argv[])
         p = p->urm;
     }
     volatilitate = (sqrt(volatilitate/(n-1)));
+    S = trunc((rand_mediu/volatilitate)*1000)/1000.0;
     rand_mediu = trunc(rand_mediu*1000)/1000.0;
     volatilitate = trunc(volatilitate*1000)/1000.0;
-    fprintf(out, "%.3lf\n", rand_mediu);
-    fprintf(out, "%.3lf\n", volatilitate);
+    fprintf(out,"%.3lf\n",rand_mediu);
+    fprintf(out,"%.3lf\n",volatilitate);
+    fprintf(out,"%.3lf",S);
     fclose(in);
     fclose(out);
     return 0;
