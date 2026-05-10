715164719a345278b22096a4cc10f82d2aa2eb1d
Miloiu Raluca-Maria <miloiuralucamaria@gmail.com>
2026-05-10 14:52:19 +0000

    Modificare:mutare structuri+functii in referinte.c si referinte.h

--- src/referinte.c
+++ src/referinte.c
@@ -1,4 +1,6 @@
 #include "referinte.h"
+//TASK2:
+
 //PUSH,STIVA
 void push(P**cap,float n)
 {
@@ -74,4 +76,69 @@ void rem_coada(OP*q,FILE *out)
         q->left = NULL;
     fprintf(out,"ziua %d - %.2f - %s",data,d,sir);
     free (aux);
+}
+
+//TASK3:
+
+
+N *Nou_nou(char data[5], int ad)
+{
+    N* nod = (N*) malloc (sizeof(N));
+    nod->actiuni = (L*)malloc(sizeof(L));
+    strcpy(nod->actiuni->simbol, data);
+    nod->actiuni->next = NULL;
+    nod->stanga = nod->dreapta = NULL;
+    nod->adancime = ad;
+    return nod;
+}
+
+void add_lista(N* nod, L* nou, char data[5])
+{
+    L *p = nod->actiuni;
+    while(p->next != NULL) p = p->next;
+    p->next = nou;
+    p = nou;
+}
+
+N* insert(N* nod, char data[5], int ziua_finala, int actiune, float pret[50][50], int adancime)
+{
+    if(nod == NULL) nod = Nou_nou(data, adancime);
+    else if(nod->adancime == adancime)
+    {
+        L* nou = (L*)malloc(sizeof(L));
+        nou->next = NULL;
+        strcpy(nou->simbol, data);
+        add_lista(nod, nou, data);
+    }
+    if(adancime>=ziua_finala)return nod;
+    if(pret[adancime+1][actiune] < pret[adancime][actiune]) 
+        nod->stanga = insert(nod->stanga, data, ziua_finala, actiune, pret, adancime+1);
+    else
+        nod->dreapta = insert(nod->dreapta, data, ziua_finala, actiune, pret, adancime+1);
+    return nod;
+}
+
+void traseu(N* root, char nume[11][5], int tr[11][11], int m, int indice)
+{
+    if(root == NULL)return;
+    if(indice != 0)
+    {
+        L *p = root->actiuni;
+        while(p!=NULL)
+        {
+            int i=0;
+            while(i<m)
+            {
+                if(strcmp(nume[i], p->simbol) == 0)
+                {
+                    tr[(root->adancime)-1][i] = indice;
+                    break;
+                }
+                i++;
+            }
+            p = p->next;
+        }
+    }
+    traseu(root->stanga, nume, tr, m, 1);
+    traseu(root->dreapta, nume, tr, m, 2);
 }
--- src/referinte.h
+++ src/referinte.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+//TASK2:
 typedef struct piata
 {
     float nr;
@@ -24,3 +25,26 @@ float pop(P **cap);
 OP * creare_coada();
 void add_coada(OP*q,int data,float v,char sir[30]);
 void rem_coada(OP*q,FILE *out);
+
+
+//TASK3:
+typedef struct Lista
+{
+    char simbol[5];
+    int ziua;
+    struct Lista *next;
+}L;
+
+typedef struct Nod
+{
+    L *actiuni;
+    struct Nod *stanga;
+    struct Nod *dreapta;
+    int adancime;
+}N;
+N *Nou_nou(char data[5], int ad);
+void add_lista(N* nod, L* nou, char data[5]);
+N* insert(N* nod, char data[5], int ziua_finala, int actiune, float pret[50][50], int adancime);
+void traseu(N* root, char nume[11][5], int tr[11][11], int m, int indice);
+
+
